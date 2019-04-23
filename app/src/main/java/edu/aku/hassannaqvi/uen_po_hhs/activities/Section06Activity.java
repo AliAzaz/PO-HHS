32cb096fcda21d559e7a27091e63979e7d61bfcb
Mustufa786 <mustufa_ayub@hotmail.com>
2019-04-23 11:19:05 +0500

    section 06 done

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs/activities/Section06Activity.java
@@ -0,0 +1,15 @@
+package edu.aku.hassannaqvi.uen_po_hhs.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.uen_po_hhs_fl.R;
+
+public class Section06Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section06);
+    }
+}
