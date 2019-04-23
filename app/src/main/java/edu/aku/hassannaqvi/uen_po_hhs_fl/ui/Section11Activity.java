656bd8a498ac47069ff22b411968b69a946a886e
Mustufa786 <mustufa_ayub@hotmail.com>
2019-04-23 14:05:57 +0500

    section 08,09,10,11 done

--- app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs_fl/ui/Section11Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs_fl/ui/Section11Activity.java
@@ -5,11 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 
 import edu.aku.hassannaqvi.uen_po_hhs_fl.R;
 
-public class Section02_03Activity extends AppCompatActivity {
+public class Section11Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section02_03);
+        setContentView(R.layout.activity_section11);
     }
 }
