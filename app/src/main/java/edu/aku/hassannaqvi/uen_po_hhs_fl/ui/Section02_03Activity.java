ddaa156233e82579a3dadea435c8c0d88f022833
abdul.sajid <indus1530@gmail.cm>
2019-04-23 15:23:40 +0500

    almost done 4 & 5

--- app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs_fl/ui/Section02_03Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs_fl/ui/Section02_03Activity.java
@@ -1,15 +1,21 @@
 package edu.aku.hassannaqvi.uen_po_hhs_fl.ui;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import edu.aku.hassannaqvi.uen_po_hhs_fl.R;
+import edu.aku.hassannaqvi.uen_po_hhs_fl.databinding.ActivitySection0203Binding;
 
 public class Section02_03Activity extends AppCompatActivity {
 
+    ActivitySection0203Binding bi;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section02_03);
-    }
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    bi = DataBindingUtil.setContentView(this, R.layout.activity_section02_03);
+    bi.setCallback(this);
+    this.setTitle("Command AND Control Centre");
+}
 }
