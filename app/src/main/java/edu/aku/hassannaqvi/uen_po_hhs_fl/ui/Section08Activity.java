c8393006ba2b32c88d0eb5e81c10ac8ccfa1789d
Mustufa786 <mustufa_ayub@hotmail.com>
2019-04-23 15:16:13 +0500

    section 08,09,10,11 done

--- app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs_fl/ui/Section08Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs_fl/ui/Section08Activity.java
@@ -1,15 +1,53 @@
 package edu.aku.hassannaqvi.uen_po_hhs_fl.ui;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import org.json.JSONException;
 
 import edu.aku.hassannaqvi.uen_po_hhs_fl.R;
+import edu.aku.hassannaqvi.uen_po_hhs_fl.databinding.ActivitySection08Binding;
 
 public class Section08Activity extends AppCompatActivity {
+
+    ActivitySection08Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section08);
+
+         bi = DataBindingUtil.setContentView(this,R.layout.activity_section08);
+    }
+
+    public void BtnContinue() {
+        if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+                Toast.makeText(this, "Starting 2nd Section", Toast.LENGTH_SHORT).show();
+//                MainApp.endActivity(this, this, Qoc2.class, true, RSDInfoActivity.fc);
+
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean UpdateDB() {
+
+        return true;
+    }
+
+    private void SaveDraft() {
+    }
+
+    private boolean formValidation() {
+
+        return true;
     }
 }
--- app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs_fl/ui/Section09_10Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs_fl/ui/Section09_10Activity.java
@@ -1,15 +1,53 @@
 package edu.aku.hassannaqvi.uen_po_hhs_fl.ui;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import org.json.JSONException;
 
 import edu.aku.hassannaqvi.uen_po_hhs_fl.R;
+import edu.aku.hassannaqvi.uen_po_hhs_fl.databinding.ActivitySection0910Binding;
 
 public class Section09_10Activity extends AppCompatActivity {
+
+    ActivitySection0910Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section09_10);
+
+        bi = DataBindingUtil.setContentView(this,R.layout.activity_section09_10);
+    }
+
+    public void BtnContinue() {
+        if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+                Toast.makeText(this, "Starting 2nd Section", Toast.LENGTH_SHORT).show();
+//                MainApp.endActivity(this, this, Qoc2.class, true, RSDInfoActivity.fc);
+
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean UpdateDB() {
+
+        return true;
+    }
+
+    private void SaveDraft() {
+    }
+
+    private boolean formValidation() {
+
+        return true;
     }
 }
