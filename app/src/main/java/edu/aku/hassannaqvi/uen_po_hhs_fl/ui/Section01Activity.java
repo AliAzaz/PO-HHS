c3ceb3e1b9b19c96da166f6f16744d88d81267d5
Mustufa786 <mustufa_ayub@hotmail.com>
2019-04-23 15:44:33 +0500

    minor fixes

--- app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs_fl/ui/Section01Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs_fl/ui/Section01Activity.java
@@ -3,6 +3,9 @@ package edu.aku.hassannaqvi.uen_po_hhs_fl.ui;
 import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.json.JSONException;
 
 import edu.aku.hassannaqvi.uen_po_hhs_fl.R;
 import edu.aku.hassannaqvi.uen_po_hhs_fl.databinding.ActivitySection01Binding;
@@ -14,13 +17,63 @@ public class Section01Activity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        bi = DataBindingUtil.setContentView(this,R.layout.activity_section_01);
-
-
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_01);
 
 
     }
 
+    public void BtnContinue() {
+        if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+                Toast.makeText(this, "Starting 2nd Section", Toast.LENGTH_SHORT).show();
+//                MainApp.endActivity(this, this, Qoc2.class, true, RSDInfoActivity.fc);
 
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean UpdateDB() {
+
+        return true;
+    }
+
+    private void SaveDraft() {
+    }
+
+    private boolean formValidation() {
+
+        return true;
+    }
+
+    public void BtnEnd() {
+        if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+                Toast.makeText(this, "Starting 2nd Section", Toast.LENGTH_SHORT).show();
+//                MainApp.endActivity(this, this, Qoc2.class, true, RSDInfoActivity.fc);
+
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        Toast.makeText(this, "You can't go back", Toast.LENGTH_SHORT).show();
+    }
 }
 
