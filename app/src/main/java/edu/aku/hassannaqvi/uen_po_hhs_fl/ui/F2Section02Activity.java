c4549b485117f44d393323176910ad776af9c28d
Mustufa786 <mustufa_ayub@hotmail.com>
2019-04-24 15:24:01 +0500

    Form 02 Section 01 02 added

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs_fl/ui/F2Section02Activity.java
@@ -0,0 +1,66 @@
+package edu.aku.hassannaqvi.uen_po_hhs_fl.ui;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import edu.aku.hassannaqvi.uen_po_hhs_fl.R;
+import edu.aku.hassannaqvi.uen_po_hhs_fl.core.MainApp;
+import edu.aku.hassannaqvi.uen_po_hhs_fl.databinding.ActivityF2Section02Binding;
+import edu.aku.hassannaqvi.uen_po_hhs_fl.validator.ValidatorClass;
+
+public class F2Section02Activity extends AppCompatActivity {
+
+    ActivityF2Section02Binding bi;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_f2_section02);
+    }
+
+    public void BtnContinue() {
+        if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+                Toast.makeText(this, "Starting 2nd Section", Toast.LENGTH_SHORT).show();
+//                MainApp.endActivity(this, this, Qoc2.class, true, RSDInfoActivity.fc);
+
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean UpdateDB() {
+
+        return true;
+    }
+
+    private void SaveDraft() {
+    }
+
+    private boolean formValidation() {
+
+        return ValidatorClass.EmptyCheckingContainer(this, bi.f2Section02);
+    }
+
+    public void BtnEnd() {
+        MainApp.endActivity(this, this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        Toast.makeText(this, "You can't go back", Toast.LENGTH_SHORT).show();
+    }
+}
