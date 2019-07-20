a74fb83b40a8eb55bbb294ecdad60159a4ee53c9
syed.irfanullah <irfansyed.pg@gmail.com>
2019-07-20 17:04:07 +0500

    Checked Validation

--- app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs_fl/ui/form4/F4Section01Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_po_hhs_fl/ui/form4/F4Section01Activity.java
@@ -1,5 +1,6 @@
 package edu.aku.hassannaqvi.uen_po_hhs_fl.ui.form4;
 
+import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
@@ -7,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -14,15 +17,23 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import edu.aku.hassannaqvi.uen_po_hhs_fl.R;
 import edu.aku.hassannaqvi.uen_po_hhs_fl.contracts.FormsContract;
+import edu.aku.hassannaqvi.uen_po_hhs_fl.contracts.LHWContract;
+import edu.aku.hassannaqvi.uen_po_hhs_fl.contracts.TalukasContract;
+import edu.aku.hassannaqvi.uen_po_hhs_fl.contracts.UCsContract;
+import edu.aku.hassannaqvi.uen_po_hhs_fl.contracts.VillagesContract;
 import edu.aku.hassannaqvi.uen_po_hhs_fl.core.DatabaseHelper;
 import edu.aku.hassannaqvi.uen_po_hhs_fl.core.MainApp;
 import edu.aku.hassannaqvi.uen_po_hhs_fl.databinding.ActivityF3Section01Binding;
 import edu.aku.hassannaqvi.uen_po_hhs_fl.databinding.ActivityF4Section01Binding;
 import edu.aku.hassannaqvi.uen_po_hhs_fl.ui.other.EndingActivity;
+import edu.aku.hassannaqvi.uen_po_hhs_fl.utils.DateUtils;
 import edu.aku.hassannaqvi.uen_po_hhs_fl.validator.ClearClass;
 import edu.aku.hassannaqvi.uen_po_hhs_fl.validator.ValidatorClass;
 
@@ -30,24 +41,128 @@ import edu.aku.hassannaqvi.uen_po_hhs_fl.validator.ValidatorClass;
 public class F4Section01Activity extends AppCompatActivity {
 
     ActivityF4Section01Binding bi;
+    private List<String> ucName, talukaNames, villageNames, lhwNames;
+    private List<String> ucCode, talukaCodes, villageCodes, lhwCodes;
+
     DatabaseHelper db;
     String dtToday = new SimpleDateFormat("dd-MM-yy HH:mm").format(new Date().getTime());
+
+    public static String DOB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bi = DataBindingUtil.setContentView(this, R.layout.activity_f4_section01);
-       // bi.setCallback(this);
-       // this.setTitle(R.string.pofisec1);
-
+        bi.setCallback(this);
+        this.setTitle(R.string.pohrainfo);
         db = new DatabaseHelper(this);
-        events_call();
+
+
+        populateSpinner(this);
+       // events_call();
 
         clickListener();
+
+    }
+
+    public void populateSpinner(final Context context) {
+        // Spinner Drop down elements
+        talukaNames = new ArrayList<>();
+        talukaCodes = new ArrayList<>();
+
+        talukaNames.add("....");
+        talukaCodes.add("....");
+
+        Collection<TalukasContract> dc = db.getAllTalukas();
+
+        for (TalukasContract d : dc) {
+            talukaNames.add(d.getTaluka());
+            talukaCodes.add(d.getTalukacode());
+        }
+
+        bi.pohra01.setAdapter(new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, talukaNames));
+
+        bi.pohra01.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if (position == 0) return;
+
+                ucCode = new ArrayList<>();
+                ucName = new ArrayList<>();
+
+                ucCode.add("....");
+                ucName.add("....");
+
+                Collection<UCsContract> pc = db.getAllUCsbyTaluka(talukaCodes.get(position));
+                for (UCsContract p : pc) {
+                    ucCode.add(p.getUccode());
+                    ucName.add(p.getUcs());
+                }
+
+                bi.pohra02.setAdapter(new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, ucName));
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+
+            }
+        });
+
+        bi.pohra02.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if (position == 0) return;
+
+                villageCodes = new ArrayList<>();
+                villageNames = new ArrayList<>();
+
+                villageCodes.add("....");
+                villageNames.add("....");
+
+                Collection<VillagesContract> pc =
+                        db.getAllPSUsByTaluka(talukaCodes.get(bi.pohra01.getSelectedItemPosition()),
+                                ucCode.get(bi.pohra02.getSelectedItemPosition()));
+                for (VillagesContract p : pc) {
+                    villageCodes.add(p.getVillagecode());
+                    villageNames.add(p.getVillagename());
+                }
+
+                lhwCodes = new ArrayList<>();
+                lhwNames = new ArrayList<>();
+
+                lhwCodes.add("....");
+                lhwNames.add("....");
+
+                Collection<LHWContract> lhw =
+                        db.getAllLHWsByTaluka(talukaCodes.get(bi.pohra01.getSelectedItemPosition()),
+                                ucCode.get(bi.pohra02.getSelectedItemPosition()));
+                for (LHWContract p : lhw) {
+                    lhwCodes.add(p.getLhwcode());
+                    lhwNames.add(p.getLhwname());
+                }
+
+                bi.pohra03.setAdapter(new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, lhwNames));
+                bi.pohra05.setAdapter(new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, villageNames));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
     }
 
 
+
     private void clickListener() {
+
 
 
     }
@@ -55,26 +170,78 @@ public class F4Section01Activity extends AppCompatActivity {
 
     public void BtnContinue() {
 
+        if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+                finish();
+                startActivity(new Intent(this, EndingActivity.class).putExtra("complete", true));
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
+
 
     public void BtnEnd() {
 
 
+        try {
+            SaveDraft();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (UpdateDB()) {
+            finish();
+            startActivity(new Intent(this, EndingActivity.class).putExtra("complete", false));
+        } else {
+            Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 
 
+    private boolean UpdateDB() {
+        long updcount = db.addForm(MainApp.fc);
+
+        MainApp.fc.set_ID(String.valueOf(updcount));
+        if (updcount != 0) {
+            MainApp.fc.set_UID(
+                    (MainApp.fc.getDeviceID() + MainApp.fc.get_ID()));
+            db.updateFormID();
+            return true;
+        } else {
+            Toast.makeText(this, "Updating Database... ERROR!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+
+
+    public boolean formValidation() {
+
+        return ValidatorClass.EmptyCheckingContainer(this, bi.llform04);
+    }
 
     private void SaveDraft() throws JSONException {
 
     }
 
 
-    void events_call() {
-
-
+    void events_call()
+    {
 
     }
+
+
+
+
 
 }
 
